a4d321fd23dde653ac96f322008d7cb6df8f5190
poulamikundu123 <kundupoulami043@gmail.com>
2025-08-07 19:22:09 +0000

    datatypes knowledge

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version // pehle wale toh browser mei inbuilt rehta tha
+
+// alert (3+3) // we are using nodejs,not browser
+
+console.log(3
+    +
+     3) // code readability should be high
+    
+console.log("poulami")
+
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol=> unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
