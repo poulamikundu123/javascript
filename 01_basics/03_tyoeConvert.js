9e2546c51a3845a5b57375393a0bb7bb3910791a
poulamikundu123 <kundupoulami043@gmail.com>
2025-08-07 20:14:07 +0000

    basic operations

--- 01_basics/03_tyoeConvert.js
+++ 01_basics/03_tyoeConvert.js
@@ -1,4 +1,4 @@
-let score = 23
+/*let score = 23
 let score1 = "23"
 //const {score} = req.body
 
@@ -37,5 +37,59 @@ let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
 console.log(stringNumber); //dikhne mei number jaisa hai but string ban chuka hai
 
+*/
+// ********************** OPERATIONS ********************
 
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/* BASIC MATH OPERATIONS
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+*/
+
+let str1 = "poulami"
+let str2 = " kundu"
+
+let str3 = str1 + str2 //add hi hota hai bss, not substract
+console.log(str3);
+
+console.log("poulami" + "kundu");
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log("1" + 2 + 2); // output 122
+console.log(1 + "2" + 2); // output 122
+console.log(1 + 2 + "2"); // output 32  1+2 =3 and then 2 is getting add
+
+console.log((1 + 2)+ "2"); //wahi bss code readability ko improve, ki kaunsa execution kaisa
+                          // console.log((3+4) * 3 % 4);
+
+console.log(true);
+console.log(+true); //+ krne se value mei convert hojaraha hai
+console.log(+"");
+
+let num1, num2 , num3
+
+num1= num2 = num3= 2+2 //not advised kyuki readability, better more ways to declarre it
+
+let gameCounter = 100
+gameCounter++ ;
+console.log(gameCounter);
+
+
+// postfix & prefix increment
+let a=3;
+const b=a++;
+console.log(a,b);
+
+let x=3;
+const y=++x;
+console.log(x,y);
 
