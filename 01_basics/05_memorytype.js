2e28d9632680374dbd33971735b90228941623c2
poulamikundu123 <kundupoulami043@gmail.com>
2025-08-10 14:20:58 +0000

    stacks&heaps

--- 01_basics/05_memorytype.js
+++ 01_basics/05_memorytype.js
@@ -3,4 +3,19 @@
 let myinsta = "Poulamikundu"
 
 let anotherinsta = myinsta
+anotherinsta = "priyankakundu"
 console.log(anotherinsta);
+console.log(myinsta);
+
+
+let account = {
+    accountID: "baniKundu",
+    instaId: "sneha@gmail.com" ,
+    number: 9734927372
+}
+
+let accountID2= account
+accountID2.accountID=  "ashutoshKundu"
+console.log(account.accountID);
+console.log(accountID2.accountID);
+
