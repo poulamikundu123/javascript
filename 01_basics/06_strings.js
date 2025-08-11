1ed10c088ba9397a65dda42b5df48b510fcd830a
poulamikundu123 <kundupoulami043@gmail.com>
2025-08-11 17:09:58 +0000

    string operations

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,10 +1,10 @@
-const name = "poulami"
+/*const name = "poulami"
 const repocount = 50
 
 console.log(name + repocount + "KUNDU"); // this type of syntax is asked to avoid
 
 console.log(`hello my name is ${name} and my repocount is ${repocount}`);
-
+*/
 
 const gamename = new String('poulami-kundu') // 0-'p' 1- 'o'......here dont think it iss an array, object ki tarah store hua hai.
 
@@ -17,6 +17,26 @@ console.log(gamename.charAt(5));
 console.log(gamename.indexOf('p'));
 console.log(gamename.charAt(5));
 
-const newString = gamename.substring(0,5)
+const newString = gamename.substring(0,5) // substring mei -ve daalo bhi toh it will ignore it, 0 se hi start karega vo toh
 console.log(newString);
+
+
+const anotherString = gamename.slice(-13,8) //best part about slice is it can accomodate negatives//-13 meaning peeche se start karega
+console.log(anotherString);
+
+const newStringOne = "   hitesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim()); //to avoid the unnessary gaps mistakenly input by the user..ignores the starting and end gaps
+
+const url = "https://hitesh.com/hitesh%20kundu"
+console.log(url);
+
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('hitesh'));
+
+console.log(gamename.split('-'));// we can split based on anything
+console.log(gamename.split('k'));
+
 
