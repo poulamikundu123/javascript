afa2309131ca27c3b3c0b50765d441d4bf2e1909
poulamikundu123 <kundupoulami043@gmail.com>
2025-09-04 02:18:24 +0000

    lecture 16 done

--- 01_basics/02_basics/02_objects.js
+++ 01_basics/02_basics/02_objects.js
@@ -5,10 +5,17 @@ but agar constructor se banta hai toh humesha singleton
 
 
 // object literals
+
+const mySym = Symbol("key1") //interview Q: ak symbol lo aaur usko object  ke key mei add krke dikhado
+
+const mySym2 = Symbol("key2")
+
 const JsUser = {
     name: "poulami", //object humhe key and value dono rakhne ka mauka deta hai jisse hum usse access kr paaye,unlike arrays jiskobss index ke trough hi kr paaatey hai
     age: 18,
     "full name": "poulami kundu",
+    mySym: "mykey1",
+    [mySym2]: "mykey1",
     location: "siliguri",
     email: "poulamikundu103@gmail.com",
     isLoggedIn: false,
@@ -19,3 +26,29 @@ console.log(JsUser.email);
 console.log(JsUser["email"]);
 console.log(JsUser["full name"]); //the only option to print in this case 
 
+console.log(typeof JsUser.mySym); // string aaraha hoga, isnt accessed as a symbol defined above
+console.log(JsUser[mySym2]); //value mei toh fharaq nahi padha hai but this is the correct syntax to implement a symbol
+
+
+//Object.freeze(JsUser)
+JsUser.email = 'priyanka123@gmail.com'
+console.log(JsUser.email);
+
+
+//function ko aap ak variable ke jaisa treat krlo koi tension nhi
+JsUser.greeting = function(){
+    console.log("hello JS user");
+    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`hello JS user,${this["full name"]}`);//when we need to take sam object ka hi refernce... this. ab jo bhi properites hai objects ke andar sab mil jayegi
+    
+}//this ko basically objects ke andar kya kya property hai usko pata krne ke liye bhi use krte hai
+
+
+console.log(JsUser.greeting); //function execute nhi huwa hai, but function ka refernce return back huwa hai
+console.log(JsUser.greeting()); 
+
+console.log(JsUser.greetingTwo());
+console.log(JsUser.greetingTwo);
